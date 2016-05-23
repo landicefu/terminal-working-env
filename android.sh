1cc36414d7bba8d9bf153bc27b8c4778ea4e9bf2
Landice Fu <landicefu@kkbox.com>
2016-05-23 10:09:18 +0800

    add clear_package_data() and clear_studio_package()

--- android.sh
+++ android.sh
@@ -10,3 +10,21 @@ function logcat_monitor()
 		fi
 	done
 }
+
+function clear_package_data()
+{
+	echo ${Red}Package Name: ${Yellow}$1${Reset}
+	echo ${Red}Clearing package data...${Reset}
+	adb shell  pm clear $1
+}
+
+function clear_studio_package()
+{
+	temp_path=$(pwd)
+	croot
+	cd app/src/
+	package_name=$(find . -name 'AndroidManifest.xml' | xargs grep package | sed -e 's/.*package=\"//' -e 's/\".*//')
+	clear_package_data $package_name
+	cd "$temp_path"
+}
+
