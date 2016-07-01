72cb74aa838d4f52e5c2abcb2dcc8d787f7455b2
Landice Fu <landicefu@kkbox.com>
2016-07-01 13:50:06 +0800

    temp

--- android.sh
+++ android.sh
@@ -37,3 +37,51 @@ function uninstall_studio_package()
 {
 	adb shell pm uninstall $(get_studio_package)
 }
+
+function adbs_device_ids()
+{
+	adb devices | grep device | grep -v attached | awk '{print $1}'
+}
+
+function adbs_select()
+{
+	devices=$(adbs_device_ids)
+	num_device=$(echo $devices | wc -l | sed -e 's/^[[:space:]]*//')
+
+	case "$num_device" in
+		0)	echo "There is no device to be selected"
+			rm ~/.tmp/adbs_selected
+			;;
+		1)	echo $devices > ~/.tmp/adbs_selected
+			;;
+		*)	echo "There are more than two devices:"
+			echo $devices
+			;;
+	esac
+
+}
+
+function fill_external_storage()
+{
+	avail=$(adb shell df | grep \/data | awk '{print $4}')
+	i=$((${#avail}-1))
+	unit="${avail:$i:1}"
+	value="${avail:0:i}"
+
+	if [ "$unit" == "G" ]; then
+		bytes=$(echo "$value*1024*1024*1024" | bc)
+	elif [ "$unit" == "M" ]; then
+		bytes=$(echo "$value*1024*1024" | bc)
+	elif [ "$unit" == "K" ]; then
+		bytes=$(echo "$value*1024" | bc)
+	fi
+
+	bytes=$(printf %.0f $(echo "$bytes"))
+
+	if [ "$bytes" -gt "1073741824" ]; then
+		gb_count=$(echo "$(printf %.0f $(echo "$bytes/1073741824" | bc))-1" | bc)
+		echo $gb_count
+	fi
+	
+}
+
