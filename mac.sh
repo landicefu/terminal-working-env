cb37193f6014d84060783379b8110ba69285e5b0
Landice Fu <landicefu@kkbox.com>
2017-04-19 12:03:26 +0800

    remove duplicated path

---
+++ mac.sh
@@ -0,0 +1,15 @@
+function remove_duplicated_path() {
+  if [ -n "$PATH" ]; then
+    old_PATH=$PATH:; PATH=
+    while [ -n "$old_PATH" ]; do
+      x=${old_PATH%%:*}       # the first remaining entry
+      case $PATH: in
+        *:"$x":*) ;;         # already there
+        *) PATH=$PATH:$x;;    # not there yet
+      esac
+      old_PATH=${old_PATH#*:}
+    done
+    PATH=${PATH#:}
+    unset old_PATH x
+  fi
+}
--- source.sh
+++ source.sh
@@ -1,6 +1,10 @@
 source_dir=$(dirname $BASH_SOURCE)
+source $source_dir/mac.sh
 source $source_dir/source_navigation.sh
 source $source_dir/android.sh
 source $source_dir/search.sh
 
-alias tupdate="source ~/.bash_profile"
+function tupdate() {
+	source ~/.bash_profile
+	remove_duplicated_path
+}
