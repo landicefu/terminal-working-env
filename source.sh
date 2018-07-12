581b63a73426c6cbdad1a83f5a9a5dacf2b86cd2
Landice Fu <landice.fu@honestbee.com>
2018-07-12 14:55:59 +0800

    Add local.sh

--- source.sh
+++ source.sh
@@ -3,6 +3,7 @@ source $source_dir/mac.sh
 source $source_dir/source_navigation.sh
 source $source_dir/android.sh
 source $source_dir/search.sh
+source $source_dir/local.sh
 export PATH=$PATH:$source_dir/bin
 
 function tupdate() {
